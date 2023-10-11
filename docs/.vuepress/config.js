4d3645e2ee9ba3f24001f452cf754fc6ec3cc8fe
biantao <tbian@topxgun.com>
2023-10-11 15:15:00 +0800

    搜索

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,56 @@
 import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from 'vuepress'
+import { searchPlugin } from '@vuepress/plugin-search'
 
 export default ({
   lang: 'zh-CN',
   title: 'doc-ccc2',
-  description: '这是我的第一个 VuePress 站点',
+  description: '个人知识库',
   theme: defaultTheme({
 	  locales :{
-		  '/':{
+		'/':{
 			colorMode : 'auto',
 			colorModeSwitch:true,
-		  navbar: [
-			  // NavbarItem
-			  {
-				text: 'Foo',
-				link: '/2222/1.md',
-			  },
-			  // NavbarGroup
-			  {
-				text: 'Group',
-				children: ['/group/foo.md', '/group/bar.md'],
-			  },
-			  // 字符串 - 页面文件路径
-			  '/bar/README.md',
-			],
-			
+			navbar: [
+				// NavbarItem
+				{
+					text: 'Foo',
+					link: '/2222/1.md',
+				},
+				// NavbarGroup
+				{
+					text: 'Group',
+					children: ['/group/foo.md', '/group/bar.md'],
+				},
+				// 字符串 - 页面文件路径
+				'/bar/README.md',
+				],
+
 			sidebar: {
-      '/': [
-        {
-          text: 'Guide',
-          children: ['/README.md', '/1.md'],
-        },
-      ],
-      '/reference/': [
-        {
-          text: 'Reference',
-          children: ['/reference/cli.md', '/reference/config.md'],
-        },
-      ],
-    },
-		  }
+				'/': [
+					{
+						text: 'Guide',
+						children: ['/README.md', '/1.md'],
+					},
+				],
+				'/reference/': [
+					{
+						text: 'Reference',
+						children: ['/reference/cli.md', '/reference/config.md'],
+					},
+				],
+			},
+		}
 	  }
 
   }),
+  plugins: [
+    searchPlugin({
+		locales: {
+			'/': {
+			  placeholder: '搜索',
+			},
+		  },
+    }),
+  ],
 })
